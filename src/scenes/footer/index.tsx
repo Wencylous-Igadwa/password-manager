56e445e01c82e7457dd39739c7a6cfcee07065a5
Wencylous-Igadwa <117176458+Wencylous-Igadwa@users.noreply.github.com>
2024-10-23 00:39:25 +0300

    Add files via upload

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,28 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-black py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <p className="my-5">
+             
+             wency
+
+          </p>
+          <p>{/*Dont Forget to Enter Text Here*/}</p>
+        </div>
+        <div>
+
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0" >
+          <h4 className="font-bold text-white" >Contact Us</h4>
+          <p className="my-5 text-white" >Contact The Admin</p>
+          <p >0713661864</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
